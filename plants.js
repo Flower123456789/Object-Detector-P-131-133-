e7c550254334bd32b9f56ed81e8914cdf908186e
Flower123456789 <82616100+Flower123456789@users.noreply.github.com>
2021-12-16 07:35:53 -0600

    Add files via upload

--- plants.js
+++ plants.js
@@ -1,6 +1,7 @@
 
 img = "";
 status = "";
+objects = [];
 
 
 function preload() {
@@ -22,7 +23,23 @@ function modelLoaded() {
 }
 
 function draw() {
-    image(img, 0, 0, 640, 420);
+
+    image( img, 0, 0, 640, 420);
+
+    if(status != "") {
+
+        for( i = 0; i < objects.length; i++ ) {
+
+            document.getElementById("status").innerHTML = " Status: Object Detected";
+            
+            fill("#03f4fc");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "  " + percent + "%", objects[i].x ,objects[i].y );
+            noFill();
+            stroke("royalblue");
+            rect(objects[i].x ,objects[i].y ,objects[i].width ,objects[i].height);
+        }
+    }
 }
 
 
